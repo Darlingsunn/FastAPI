24569c0cb3d85c6c37c1a87b08858dca80138709
Aidar <newlifeaidar@gmail.com>
2025-04-13 22:43:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,19 +42,6 @@ def delete_hotel(hotel_id: int):
     return {'status': 'OK'}
 
 
-def update_hotel(hotels: list,
-                 hotel_id: int,
-                 title: str,
-                 name: str):
-    for hotel in hotels:
-        if hotel['id'] == hotel_id:
-            if hotel['title'] is not None:
-                hotel['title'] = title
-            if hotel['name'] is not None:
-                hotel['name'] = name
-            return {"status": "ok"}
-        return {"error": "try again"}
-
 @app.put("/hotels/{hotel_id}")
 def put_hotel(
         hotel_id: int,
@@ -62,26 +49,34 @@ def put_hotel(
         name: str = Body(),
 ):
     global hotels
-    return update_hotel(hotels, hotel_id, title, name)
-
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    hotel["title"] = title
+    hotel["name"] = name
+    return {"status": "OK"}
 
 @app.patch("/hotels/{hotel_id}")
 def patch_hotel(hotel_id: int,
                 title: str | None = Query(default=None),
                 name: str | None = Query(default=None),
                 ):
-    return update_hotel(hotels, hotel_id, title, name)
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if title:
+        hotel["title"] = title
+    if name:
+        hotel["name"] = name
+    return {"status": "OK"}
 
 
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-     return get_swagger_ui_html(
-         openapi_url=app.openapi_url,
-         title=app.title + " - Swagger UI",
-         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
-         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
-     )
+# @app.get("/docs", include_in_schema=False)
+# async def custom_swagger_ui_html():
+#      return get_swagger_ui_html(
+#          openapi_url=app.openapi_url,
+#          title=app.title + " - Swagger UI",
+#          oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+#          swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+#          swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+#      )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
