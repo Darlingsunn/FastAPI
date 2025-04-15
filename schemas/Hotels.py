6cef2965a62aa0da49b5925ba2f409d68349749c
Aidar <newlifeaidar@gmail.com>
2025-04-15 13:45:43 +0300

    Create Hotels.py

---
+++ schemas/Hotels.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+
+
+
+class HotelPatch(BaseModel):
+    title: str | None = Field(default=None)
+    name: str | None = Field(default=None)
+
+class Hotel(BaseModel):
+    title : str
+    name : str
